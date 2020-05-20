21ddb1994888226a88156d5d271174591ba495de
Eric D Culver <eric.d.culver@gmail.com>
2020-05-20 12:42:24 -0600

    Having issues with eval step.

--- repl.py
+++ repl.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 from lark import Lark, Transformer
-import games
+from games import Game
 
 test = True
 
@@ -26,23 +26,25 @@ assignment: name ":=" expression
 name: CNAME
 
 ?expression: "{" list "|" list "}" -> general_game
-          | integer
+          | integer_game
           | dyadic_rational
           | nimber
           | up_multiple
 
 list: [ expression ("," expression)* ]
 
-integer: SIGNED_INT
+integer_game: SIGNED_INT
 
 dyadic_rational: integer "/" "2" ("^" unsigned_integer)?
 
 nimber: "*" unsigned_integer?
 
-up_multiple: "^" unsigned_integer -> up_multiple
-           | "^" unsigned_integer "*" -> upstar_multiple
-           | "v" unsigned_integer -> down_multiple
-           | "v" unsigned_integer "*" -> downstar_multiple
+up_multiple: "^" unsigned_integer? -> up_multiple
+           | "^" unsigned_integer? "*" -> upstar_multiple
+           | "v" unsigned_integer? -> down_multiple
+           | "v" unsigned_integer? "*" -> downstar_multiple
+
+integer: SIGNED_INT
 
 unsigned_integer: INT
 
@@ -59,57 +61,60 @@ parser = Lark(grammar, start='statement')
 
 class EvalStatement(Transformer):
 
-    def unsigned_integer(self, i):
-        return i
+    def unsigned_integer(self, items):
+        return int(items[0])
 
-    def integer(self, i):
-        return Game.integer(i)
+    def integer(self, items):
+        return int(items[0])
+
+    def integer_game(self, items):
+        return Game.integer(int(items[0]))
 
     def dyadic_rational(self, items):
-        num, rest* = items
+        num, *rest = items
         if rest:
-            return Game.dyadicRational(num, rest[0])
+            return Game.dyadicRational(int(num), int(rest[0]))
         else:
-            return Game.dyadicRational(num, 1)
+            return Game.dyadicRational(int(num), 1)
 
-    def nimber(self, i):
-        if i:
-            return Game.nimber(i)
+    def nimber(self, items):
+        if items:
+            return Game.nimber(int(items[0]))
         else:
             return Game.nimber(1)
 
-    def up_multiple(self, i):
-        if i:
-            return Game.upMultiple(i, 0)
+    def up_multiple(self, items):
+        if items:
+            return Game.upMultiple(int(items[0]), 0)
         else:
             return Game.upMultiple(1, 0)
 
-    def upstar_multiple(self, i):
-        if i:
-            return Game.upMultiple(i, 1)
+    def upstar_multiple(self, items):
+        if items:
+            return Game.upMultiple(int(items[0]), 1)
         else:
             return Game.upMultiple(1, 1)
 
-    def down_multiple(self, i):
-        if i:
-            return Game.upMultiple(-i, 0)
+    def down_multiple(self, items):
+        if items:
+            return Game.upMultiple(-int(items[0]), 0)
         else:
             return Game.upMultiple(-1, 0)
 
-    def downstar_multiple(self, i):
-        if i:
-            return Game.upMultiple(i, 1)
+    def downstar_multiple(self, items):
+        if items:
+            return Game.upMultiple(-int(items[0]), 1)
         else:
-            return Game.upMultiple(1, 1)
+            return Game.upMultiple(-1, 1)
 
-    def list(self, items):
-        return list(items)
+    list = list
 
     def general_game(self, items):
+        print(items)
         left, right = items
         return Game.generalGame(left, right)
 
 if test:
-    tree = parser,parse("{0, ^2*,  *17|  1, *}")
+    tree = parser.parse("{0, ^2*,  *17|  1/2, 1, v, *}")
     print(tree.pretty())
     print(EvalStatement().transform(tree))
